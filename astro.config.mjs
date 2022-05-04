2c7925b5315fc712f95446f2a43e5167af101b97
Mariana <marianaluizamr@gmail.com>
2022-05-04 10:08:27 +0100

    fix dotenv ref

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
 import tailwind from '@astrojs/tailwind';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const SITE_FOLDER = process.env.SITE_FOLDER;
 
